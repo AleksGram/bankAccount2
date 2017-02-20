2335b53c98593f6148dac4cdd4884efd91142547
AlexGram <agramatik@list.ru>
2017-02-20 22:54:29 +0200

    new project

---
+++ src/Customer.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Customer {
+    private String name;
+    private ArrayList<Double>transaction;
+
+    public Customer(String name, double initialAmount) {
+        this.name = name;
+        this.transaction=new ArrayList<Double>();
+        addTransaction(initialAmount);
+    }
+    public void addTransaction(double amount){
+        this.transaction.add(amount);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransaction() {
+        return transaction;
+    }
+    public void showCustomerTransactions(){
+        for(int i=0;i<this.transaction.size();i++){
+            System.out.println((i+1)+" tr "+transaction.get(i));
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Грам on 20.02.2017.
+ */
+public class  Main {
+    public static void main(String[] args){
+        Branch branch = new Branch("Privat");
+        branch.addNewCustomer("Silpo",100.00);
+        branch.addCustomerTransaction("Silpo",150.00);
+        branch.showTransaction("Silpo");
+    }
+
+}
